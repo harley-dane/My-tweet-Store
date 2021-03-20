e11829c2a7bae67864f7e2a7110517368093b052
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2021-03-19 18:53:38 -0600

    Create tweet.js

---
+++ tweet.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function tweet({name, message, likes}){
+return (
+<div className="tweet">
+    <h3>{name}</h3>
+    <p>{message}</p>
+    <h3>{likes}</h3>
+
+</div>
+);
+
+}
+
+
+export default tweet;
